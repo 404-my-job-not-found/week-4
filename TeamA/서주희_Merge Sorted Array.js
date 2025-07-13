78c5548e51eec436327ba3b7b806829d4f91228c
서주희 <maywngml@naver.com>
2025-07-13 14:14:22 +0900

    solve: Merge Sorted Array

---
+++ TeamA/서주희_Merge Sorted Array.js
@@ -0,0 +1,30 @@
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1,
+    j = n - 1,
+    k = nums1.length - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] < nums2[j]) {
+      nums1[k] = nums2[j];
+      j--;
+    } else {
+      nums1[k] = nums1[i];
+      i--;
+    }
+
+    k--;
+  }
+
+  if (i >= 0) {
+    for (; i >= 0; i--) {
+      nums1[k] = nums1[i];
+      k--;
+    }
+  }
+  if (j >= 0) {
+    for (; j >= 0; j--) {
+      nums1[k] = nums2[j];
+      k--;
+    }
+  }
+};
